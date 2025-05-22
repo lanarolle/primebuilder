13f19e5bdd096a00b4b9133a365067a301b08a29
NadeeshaKarunasingha <nadeeshakarunasingha@gmail.com>
2025-05-22 21:28:03 +0530

    home page title icon change

--- home.php
+++ home.php
@@ -18,7 +18,7 @@ require "connection.php";
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
 
-    <link rel="icon" href="resource/logo.svg" />
+    <link rel="icon" href="resource/1.png" />
 </head>
 
 <body>
